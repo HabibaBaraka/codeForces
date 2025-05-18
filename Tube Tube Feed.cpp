43c3ecd0db8e3f03b41aa19d17df3f924d62718d
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-05-18 10:36:43 +0300

    Create Tube Tube Feed.cpp

---
+++ Tube Tube Feed.cpp
@@ -0,0 +1,59 @@
+/*
+								  "صلي على النبي"
+			  * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا ۖ إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<queue>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+ll mod = 1e9 + 7;
+int main()
+
+{
+	//freopen("divisible.in", "r", stdin);
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	int t = 1;
+	cin >> t;
+	//SPF();
+	while (t--) {
+		int n;
+		cin >> n;
+		int tt;
+		cin >> tt;
+		vector<int>v(n), b(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+		}
+		int mx = 0, idx = -1;
+		int sk = 0;
+		for (int i = 0; i < n; i++) {
+			if (v[i] + sk<=tt) {
+				if (b[i] > mx) {
+					mx = b[i];
+					idx = i+1;
+				}
+				
+			}
+			sk++;
+		}
+		cout << idx;
+		el;
+	}
+}

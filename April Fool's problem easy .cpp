29fcbf3c5afed7f55140edd408d99b79bf196de3
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-05-23 15:54:56 +0300

    Create April Fool's problem easy .cpp

---
+++ April Fool's problem easy .cpp
@@ -0,0 +1,58 @@
+/*
+								  "صلي على النبي"
+			  * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا ۖ إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 100005
+
+
+
+int main()
+
+{
+	
+	//pre();
+	//freopen("divisible.in", "r", stdin);
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	//SPF();
+	while (t--) {
+		int n,k;
+		cin >> n>>k;
+		vector<ll>v(n);
+		
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		sort(v.begin(), v.end());
+		ll sum = 0;
+		for (int i = 0; i <min(k,n); i++) {
+			sum += v[i];
+		}
+		cout << sum;
+		el;
+	}
+}

a44bf305d29aee947a1ff62a8714da09adb26c2d
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-05-25 08:42:15 +0300

    Create Boring Apartments.cpp

---
+++ Boring Apartments.cpp
@@ -0,0 +1,60 @@
+/*
+				  "صلي على النبي"
+		* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 100005
+
+
+int cntD(int n) {
+	int cnt = 0;
+	while (n > 0) {
+		n /= 10;
+		cnt++;
+	}
+	return cnt;
+}
+int main()
+
+{
+	
+	//pre();
+	//freopen("divisible.in", "r", stdin);
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	int t = 1;
+	cin >> t;
+	//SPF();
+	while (t--) {
+		
+		int n;
+		cin >> n;
+		int d = n % 10;
+		int ans = (d - 1) * 10;
+		int over = cntD(n);
+		ans += ((over * (over + 1)) / 2);
+		cout << ans;
+		el;
+	}
+}

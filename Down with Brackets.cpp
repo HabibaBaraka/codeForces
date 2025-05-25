83bb90b62bf665a5d123e545b92202f7896675f1
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-05-25 06:32:26 +0300

    Create Down with Brackets.cpp

---
+++ Down with Brackets.cpp
@@ -0,0 +1,68 @@
+/*
+				  "صلي على النبي"
+		* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 100005
+
+
+
+int main()
+
+{
+	
+	//pre();
+	//freopen("divisible.in", "r", stdin);
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	int t = 1;
+	cin >> t;
+	//SPF();
+	while (t--) {
+		
+		string s;
+		cin >> s;
+		int cnt = 0;
+		stack<char>st;
+		for (int i = 0; i < s.size(); i++)
+		{
+			
+			if (s[i] == '(')
+			{
+				st.push(s[i]);
+			}
+			else if (!st.empty() )
+			{
+				st.pop();
+				if (st.empty())cnt++;
+			
+			}
+			
+		}
+		
+		if (cnt >= 2)cout << Y;
+		else cout << O;
+		el;
+	}
+}

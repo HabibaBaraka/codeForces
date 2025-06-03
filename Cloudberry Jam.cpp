0b826ee3b7186462fc9d0db608c3d19b103ee05e
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-06-03 17:00:55 +0300

    Create Cloudberry Jam.cpp

---
+++ Cloudberry Jam.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+#include <iostream>
+using namespace std;
+#include<map>
+#include<algorithm>
+#include<cmath>
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#include <bitset>
+using namespace std;
+
+int main()
+{
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		ll n;
+		cin >> n;
+		cout << 2 * n; el;
+	}
+}

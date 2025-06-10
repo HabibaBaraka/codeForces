64e81311d5ad86124a50d17a31071c1c774d4cbc
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-06-10 15:45:33 +0300

    Create Minority.cpp

---
+++ Minority.cpp
@@ -0,0 +1,52 @@
+/*
+          "صلي على النبي"
+    * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 100005
+int main()
+
+{
+
+    //pre();
+    //freopen("divisible.in", "r", stdin);
+    ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+    int t = 1;
+    cin >> t;
+    //SPF();
+   
+    while (t--) {
+        string s;
+        cin >> s;
+        int cnt0 = 0, cnt = 0;
+        for (char c : s)
+        {
+            c == '0' ? cnt0++ : cnt++;
+        }
+        if (cnt0 == cnt)cout << cnt-1;
+        else cout << min(cnt, cnt0);
+        el;
+    }
+}

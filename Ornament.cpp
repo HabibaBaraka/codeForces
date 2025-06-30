0cfbccaae0068790d844db9de16c6f5f0830c83a
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-06-30 12:39:33 +0300

    Create Ornament.cpp

---
+++ Ornament.cpp
@@ -0,0 +1,53 @@
+/*
+								  "صلي على النبي"
+			  * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا ۖ إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<cstring>
+#include<algorithm>
+#include<iomanip>
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<queue>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','U','D' };
+using namespace std;
+#define ll long long
+#define ld long double
+#define  graph vector<vector<int>>
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<endl;
+const ll mod = 1e9 + 7;
+const int N = 3e5 + 4;
+
+int  main()
+{
+	//freopen("input.txt", "r", stdin);    
+	//freopen("output.txt", "w", stdout);  
+
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	int t = 1;
+
+	//cin >> t;
+
+	while (t--)
+	{
+
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i & 1)cout << "R";
+				else if (j & 1)cout << "R";
+				else cout << "W";
+			}el;
+		}
+	}
+}

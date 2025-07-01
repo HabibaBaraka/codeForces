53adec668691bf9f2f172261205eac493411c5d7
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-01 14:04:26 +0300

    Create Longest Good Array.cpp

---
+++ Longest Good Array.cpp
@@ -0,0 +1,58 @@
+/*
+                  "صلي على النبي"
+        * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا ۖ إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<cstring>
+#include<algorithm>
+#include<iomanip>
+#include<unordered_map>
+#include<stack>
+#include<set>
+#include<queue>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','U','D' };
+using namespace std;
+#define ll long long
+#define ld long double
+#define  graph vector<vector<int>>
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<endl;
+const ll mod = 1e9 + 7;
+const int N = 3e5 + 4;
+bool can(ll mid,ll l,ll r)
+{
+    
+    return  l+ (mid * (mid - 1)) / 2 <= r;
+}
+int  main()
+{
+    //freopen("input.txt", "r", stdin);    
+    //freopen("output.txt", "w", stdout);  
+
+    ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+    int t = 1;
+
+    cin >> t;
+
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        ll l=1, r=m-n+1, mid,ans=1;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (can(mid, n, m)) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else r = mid - 1;
+        }
+        cout << ans;el
+    }
+}

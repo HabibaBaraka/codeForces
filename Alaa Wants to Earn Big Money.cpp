12e2b2bf069e806af7d7a2046328f3d7e0ec87bf
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-04 08:21:43 +0300

    Create Alaa Wants to Earn Big Money.cpp

---
+++ Alaa Wants to Earn Big Money.cpp
@@ -0,0 +1,53 @@
+/*
+								  "صلي على النبي"
+			  * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا ۖ إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<queue>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','U','D' };
+using namespace std;
+#define G vector<vector<int>>
+#define ll long long
+#define ld long double
+#define Y "Yes\n"
+#define O "No\n"
+#define el cout<<endl;
+const int N = 500 + 4;
+const ll mod = 1e9 + 7;
+int main()
+{
+	//freopen("input.txt", "r", stdin);    
+	//freopen("output.txt", "w", stdout);  
+
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	int t = 1;
+	//cin >> t;
+
+	while (t--)
+	{
+		int n, m, k;
+		cin >> n >> m >> k;
+		vector<int>v(n), b(m);
+
+		for (auto& it : v) {
+			cin >> it;
+		}
+		for (auto& it : b) {
+			cin >> it;
+		}
+		sort(v.begin(), v.end());
+		sort(b.begin(), b.end());
+		cout << n - (lower_bound(v.begin(), v.end(), k) - v.begin()) +
+			 (upper_bound(b.begin(), b.end(), k) - b.begin());
+	}
+}

95bcc05880356649acbba8faa41fb80245b69e9e
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-16 16:00:58 +0300

    Create Sort Array.cpp

---
+++ Sort Array.cpp
@@ -0,0 +1,51 @@
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+		  "صلي على النبي"
+	* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 100005
+#define gr vector<vector<pair<int,int>>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+int main()
+{
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		int n;
+		int k;
+		cin >> n;
+		vector<ll>v(n);
+		for (auto& it : v)cin >> it;
+		if (v[0] <= v[n - 1] || v[n - 2] <= v[n - 1] || v[0] <= v[1])cout << Y;
+		else cout << O;
+		el;
+	}
+
+	return 0;
+}

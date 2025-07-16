febe09927573c9502fc986693c8d718a329a867e
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-17 01:11:01 +0300

    Create Yogurt Sale.cpp

---
+++ Yogurt Sale.cpp
@@ -0,0 +1,52 @@
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+		  "صلي على النبي"
+	* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<cstring>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<vector>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<endl;
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 509
+#define gr vector<vector<pair<int,int>>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+
+int main()
+{
+
+	int t = 1;
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> t;
+	while (t--) {
+		int n, a, b;
+		cin >> n >> a >> b;
+		int co = (n / 2) * b;
+		if (n & 1)co += a;
+		int co2 = n * a;
+		cout << min(co2,co); el;
+	}
+
+	return 0;
+}

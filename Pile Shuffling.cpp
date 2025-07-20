1b997197f94c0378062e7cabcffbf6d1ebde7462
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-20 14:08:16 +0300

    Create Pile Shuffling.cpp

---
+++ Pile Shuffling.cpp
@@ -0,0 +1,66 @@
+/*
+								  "صلي على النبي"
+			  * قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا ۖ إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<cstring>
+#include<algorithm>
+#include<stack>
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<queue>
+const int dx[] = { 0,0,1,-1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','U','D' };
+using namespace std;
+#define G vector<vector<pair<int,ll>>>
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<endl;
+const int N = 1e5 + 4;
+const ll mod = 1e9 + 7;
+
+
+int main()
+{
+	//freopen("input.txt", "r", stdin);    
+	//freopen("output.txt", "w", stdout);  
+
+	ios_base::sync_with_stdio(0); cout.tie(0), cin.tie(0);
+	ll t = 1;
+	cin >> t;
+
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		
+		ll rec = 0;
+		ll o = 0;
+		ll ans = 0;
+		for (int i = 0; i < n; i++)
+		{
+			int a, b, c, d;
+			cin >> a >> b >> c >> d;
+			if (a < c)ans += c - a;
+			if (b < d)rec += d - b;
+			if (a > c)ans += a - c;
+			if (b > d)o += min(c,a);
+			
+		}
+		ans /= 2;
+		ans += rec;
+		ans += o;
+		cout << ans;
+		el;
+		
+	}
+}
+/*
+الحمدلله رب العالمين
+*/

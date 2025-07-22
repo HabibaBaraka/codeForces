bc25e813235575581d975127a3cf13a36dea1848
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-23 01:40:07 +0300

    Create Left and Down.cpp

---
+++ Left and Down.cpp
@@ -0,0 +1,60 @@
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+		  "صلي على النبي"
+	* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<numeric>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES"
+#define O "NO"
+#define el cout<<'\n';
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define mod = 1e9 + 7;
+#define N 100005
+#define gr vector<vector<pair<int,int>>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+	
+
+ll a,b,k;
+cin>>a>>b>>k;
+if(k>=max(a,b)||a==b)cout<<1;
+else 
+{
+  ll c=gcd(a,b);
+  if(a%c==0&&a/c<=k&&b%c==0&&b/c<=k)cout<<1;
+  else cout<<2;
+}
+
+
+el;
+	}
+
+	return 0;
+}

70962c99f8b2d0ab7f67ebb7ac54a0000a637a37
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-07-23 01:39:28 +0300

    Create  Difficult Contest.cpp

---
+++ Difficult Contest.cpp
@@ -0,0 +1,69 @@
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+		  "صلي على النبي"
+	* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<bitset>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<stack>
+#include<queue>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+#define ON(n, k) ((n) | (1 << (k)))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+//#define mod = 1e9 + 7;
+#define N 101
+#define gr vector<vector<pair<int,int>>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		int n = s.size();
+		int fr[26] = {};
+		for (char c : s)
+		{
+			fr[c - 'A']++;
+		}
+		while (fr['T' - 'A']--)
+		{
+			cout << 'T';
+		}
+		while (fr['N' - 'A']--)
+		{
+			cout << 'N';
+		}
+		while (fr['F' - 'A']--)
+		{
+			cout << 'F';
+		}
+		for (char c : s)
+		{
+			if (c != 'F' && c != 'N' && c != 'T')cout << c;
+		}el;
+	}
+	return 0;
+}

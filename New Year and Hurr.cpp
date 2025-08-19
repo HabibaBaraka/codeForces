03c8c0598a170b7cc58079e3f59958abd3d43a4f
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-08-19 14:44:23 +0300

    Create New Year and Hurr.cpp

---
+++ New Year and Hurr.cpp
@@ -0,0 +1,54 @@
+// New Level===New Part.cpp 
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+		  "صلي على النبي"
+	* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<map>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+#define Baraka ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+int main()
+{
+	Baraka
+		int t = 1;
+//	cin >> t;
+	while (t--)
+	{
+		int n, k;
+		cin >> n >> k;
+		int can = 240 - k;
+		int time = 0;
+		int x=1;
+		while (x<=n&&time < can)
+		{
+			time += 5 * x;
+			x++;
+		}
+		time>can?cout<<x-2:cout << x-1;
+
+		el;
+	}
+	return 0;
+}

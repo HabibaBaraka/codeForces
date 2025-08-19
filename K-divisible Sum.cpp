aa6b38a4cc90e38e8aa75088383ac1b2abba40ed
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-08-19 15:22:13 +0300

    Create  K-divisible Sum.cpp

---
+++ K-divisible Sum.cpp
@@ -0,0 +1,50 @@
+// New Level===New Part.cpp 
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+		  "صلي على النبي"
+	* قَالُوا سُبْحَانَكَ لَا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا  إِنَّكَ أَنتَ الْعَلِيمُ الْحَكِيمُ *
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<map>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+#define Baraka ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+int main()
+{
+	Baraka
+		int t = 1;
+	cin >> t;
+	while (t--)
+	{
+		ll n, k;
+		cin >> n >> k;
+		if (k >= n)
+		{
+			cout << (ll)ceil(k * 1.0 / n);
+		}
+		else if (n % k != 0)cout << 2;
+		else cout << 1;
+		el;
+	}
+	return 0;
+}

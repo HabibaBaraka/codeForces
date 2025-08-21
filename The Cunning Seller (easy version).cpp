8161c7565d80bf0085c8095ab3382214e63e6bdd
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-08-21 21:50:28 +0300

    Create The Cunning Seller (easy version).cpp

---
+++ The Cunning Seller (easy version).cpp
@@ -0,0 +1,73 @@
+
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+	The Cow (2:28) 
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life, 
+then He will cause you to die and again bring you to life, 
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<map>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+#define Baraka ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+
+int main()
+{
+	Baraka
+    int t = 1;
+	vector<ll>v;
+	v.push_back(1);
+	ll x = 3;
+	while (x <= 1e18)
+	{
+		v.push_back(x);
+		x *= 3;
+	}
+    cin >> t;
+	while (t--)
+	{
+		
+		ll n,ans=0;
+		cin >> n;
+		while(n)
+		{
+		  
+		auto it = upper_bound(v.begin(), v.end(), n);
+		it--;
+		int lg = it - v.begin();
+	  //	cout<<lg;
+		 ans += pow(3, lg + 1) + (lg * pow(3, lg - 1));
+		
+		n -= pow(3, lg);
+	
+		}
+		cout << ans;
+		el;
+
+	}
+	return 0;
+}

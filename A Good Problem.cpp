8284f693cc27ffdad38f6ff8bb356f053d36f997
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-08-23 17:42:38 +0300

    Create A Good Problem.cpp

---
+++ A Good Problem.cpp
@@ -0,0 +1,69 @@
+
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<map>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+#define Baraka ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+
+int main()
+{
+	Baraka
+	int t = 1;
+	
+	cin >> t;
+	while (t--)
+	{
+		ll n, l, r, k;
+		cin >> n >> l >> r >> k;
+		if (n & 1ll)cout << l;
+		else
+		{
+			ll n1, n2;
+			n1 = l;
+			ll msb = log2(l);
+			
+			msb++;
+			n2 = 1ll << msb;
+			
+			if (n2 <= r&&n>2)
+			{
+				if (k > (n - 2))cout << n2;
+				else cout << n1;
+			}
+			else cout << -1;
+		}
+
+		el
+	}
+	return 0;
+}

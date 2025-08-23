8942ec750cb71f0b39d924a915535d5d6710d3b7
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-08-23 17:43:10 +0300

    Create Add or XOR.cpp

---
+++ Add or XOR.cpp
@@ -0,0 +1,72 @@
+
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<map>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+#define Baraka ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+
+int main()
+{
+	Baraka
+	int t = 1;
+	
+	cin >> t;
+	while (t--)
+	{
+		int a, b, x, y;
+		cin >> a >> b >> x >> y;
+		if (b < a)
+		{
+			if (a & 1 && a - 1 == b)cout << y;
+			else cout << -1;
+			
+		}
+		else
+		{
+			int co = 0;
+			while (a < b)
+			{
+				if (a % 2 == 0)
+				{
+					co += min(x, y);
+				}
+				else co += x;
+
+				a++;
+			}
+			cout << co;
+			
+		}el
+	}
+	return 0;
+}

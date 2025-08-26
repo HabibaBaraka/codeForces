298585ea9155c715c90344dbe5180c60a2a5ecce
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-08-27 00:48:10 +0300

    Create Lose it !.cpp

---
+++ Lose it !.cpp
@@ -0,0 +1,87 @@
+
+//بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ 
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "N\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+#define Baraka ios_base::sync_with_stdio(0);cin.tie(0), cout.tie(0);
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e5 + 7;
+
+int main()
+{
+	Baraka
+	
+	int t = 1;
+	//cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		vector<int>v(n);
+		for (auto &it : v)cin >> it;
+		set<int>mp[43];
+		for (int i = 0; i < n; i++)
+		{
+			mp[v[i]].insert(i);
+		}
+		int cnt = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (v[i] == 4)
+			{
+				auto  a = mp[8].upper_bound(i);
+				if (a == mp[8].end())continue;
+
+				auto  b = mp[15].upper_bound(*a);
+				mp[8].erase(a);
+				if (b == mp[15].end())continue;
+
+				auto  c = mp[16].upper_bound(*b);
+				mp[15].erase(b);
+				if (c == mp[16].end())continue;
+
+				auto  d = mp[23].upper_bound(*c);
+				mp[16].erase(c);
+				if (d == mp[23].end())continue;
+
+				auto  e = mp[42].upper_bound(*d);
+				mp[23].erase(d);
+				if (e == mp[42].end())continue;
+				mp[42].erase(e);
+				cnt++;
+
+			}
+		}
+		cout<<n-(cnt*6);
+	}
+	return 0;
+}

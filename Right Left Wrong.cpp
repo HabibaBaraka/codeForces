855f364fecaf76cd95e063fcac776d9934eb990f
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-09-09 15:02:03 +0300

    Create  Right Left Wrong.cpp

---
+++ Right Left Wrong.cpp
@@ -0,0 +1,79 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<stack>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 1e9 + 7;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e7 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		
+		int n;
+		cin >> n;
+		vector<ll>v(n),pr(n);
+		for (auto& it : v)cin >> it;
+		string s;
+		cin >> s;
+		pr[0] = v[0];
+		for (int i = 1; i < n; i++)
+		{
+			pr[i] = v[i] + pr[i - 1];
+		}
+		stack<int>st;
+		for (int i=0;i<n;i++)
+		{
+			if (s[i] == 'R')st.push(i);
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (s[i] == 'L'&&!st.empty())
+			{
+				int j = st.top();
+				if (j > i)
+				{
+					ans += i > 0 ? pr[j] - pr[i - 1] : pr[j];
+					st.pop();
+				}
+			}
+		}
+		cout << ans;el
+	}
+
+	return 0;
+}

113a9a70a8d52bd93e20ba1bbcdbe7d56cbcc855
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-09-15 12:55:13 +0300

    Create  Substring and Subsequence.cpp

---
+++ Substring and Subsequence.cpp
@@ -0,0 +1,78 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 998244353;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e7 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		string st;
+		cin >> st;
+		string ss;
+		cin >> ss;
+		vector<set<int>>v(27);
+		for (int i = 0; i < st.size(); i++)
+		{
+			v[st[i] - 'a'].insert(i);
+		}
+		int len = 0;
+		int mx = 0;
+		for (int i = 0; i < ss.size(); i++)
+		{
+			    int cnt = 0;
+				int pre = -1;
+				for (int j = i; j < ss.size(); j++)
+				{
+					auto it = v[ss[j] - 'a'].upper_bound(pre);
+					if (it != v[ss[j] - 'a'].end())
+					{
+						cnt++;
+						pre = *it;
+					}
+					else break;
+				}
+			    mx = max(mx, cnt);
+		}
+
+		len = ss.size() + st.size() - mx;
+		cout << len; el
+	}
+
+	return 0;
+}

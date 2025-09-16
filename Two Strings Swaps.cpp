d030cebcfc7c4b5202d2873ac5d29e3c214db2d6
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-09-16 21:13:15 +0300

    Create Two Strings Swaps.cpp

---
+++ Two Strings Swaps.cpp
@@ -0,0 +1,82 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 998244353;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e7 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	//cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		string a,b;
+		int pre = 0;
+		cin >> a>>b;
+		for (int i = 0; i < n / 2; i++)
+		{
+			if (a[i] == b[i])
+			{
+				if (a[n - i - 1] != b[n - i - 1])pre++;
+			}
+			else if (a[n - i - 1] == b[n - i - 1])
+			{
+				if (a[i] != b[i])pre++;
+			}
+			else if (a[i] == b[n - i - 1])
+			{
+				if (b[i] != a[n - i - 1])pre++;
+			}
+			else if (b[i] == a[n - i - 1])
+			{
+				if (a[i] != b[n - i - 1])pre++;
+			}
+			else if (b[i] == b[n - i - 1])
+			{
+				if (a[i] != a[n - i - 1])pre++;
+			}
+			else pre += 2;
+		}
+		if (n & 1)
+		{
+			if (a[n / 2] != b[n / 2])pre++;
+		}
+		cout << pre;
+	}
+
+	return 0;
+}

dc0df5c3461867efafaf444e136ad1f715901d45
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-09-17 14:37:46 +0300

    Create The Strict Teacher (Hard Version).cpp

---
+++ The Strict Teacher (Hard Version).cpp
@@ -0,0 +1,69 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 998244353;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e7 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		ll n, m, q;
+		cin >> n >> m >> q;
+		vector<ll>v(m);
+		for (auto& it : v)cin >> it;
+		sort(v.begin(), v.end());
+		while (q--)
+		{
+			ll x;
+			cin >> x;
+			if (x > v[m - 1])cout << n - v[m - 1];
+			else if (x < v[0])cout << v[0] - 1;
+			else
+			{
+				auto it = lower_bound(v.begin(), v.end(), x);
+				auto itt = it;
+				itt--;
+				cout << (*it - *itt) / 2;
+			}
+			el
+		}
+		
+	}
+
+	return 0;
+}

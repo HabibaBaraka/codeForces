5e08756ecd5e31d3974d01413704cc6063241014
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-09-17 14:36:58 +0300

    Create  Simple Palindrome.cpp

---
+++ Simple Palindrome.cpp
@@ -0,0 +1,66 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 998244353;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e7 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		int a, e, i, o, u;
+		int cnt = n / 5;
+		a = cnt;
+		e = cnt; o = cnt;
+		i = cnt; u = cnt;
+		cnt = n % 5;
+		if (cnt-- > 0)a++;
+		if (cnt-- > 0)o++;
+		if (cnt-- > 0)i++;
+		if (cnt-- > 0)e++;
+		while (a--)cout << 'a';
+		while (e--)cout << 'e';
+		while (i--)cout << 'i';
+		while (u--)cout << 'u';
+		while (o--)cout << 'o';
+		el
+	}
+
+	return 0;
+}

debd3fea5ae6b4508472ff387dcb8c564e354b28
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-09-18 21:08:28 +0300

    Create Maximum Product.cpp

---
+++ Maximum Product.cpp
@@ -0,0 +1,59 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+const ll  mod = 998244353;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 1e7 + 7;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		ll n;
+		cin >> n;
+		vector<ll>v(n);
+		for (auto& it : v)cin >> it;
+		sort(v.begin(), v.end());
+		
+		ll val = v[0] * v[1] * v[2] * v[3] * v[n - 1];
+		ll val2 = v[0] * v[1] * v[n - 1] * v[n - 2] * v[n - 3];
+		ll val3 = v[n - 1] * v[n - 2] * v[n - 3] * v[n - 4] * v[n - 5];
+		cout<< max({ val,val2,val3 });
+		el
+	}
+
+	return 0;
+}

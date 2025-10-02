5586515ae9e28eefbc3e5f5bad094794172bb76b
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-10-02 18:26:02 +0300

    Create  Fall Down.cpp

---
+++ Fall Down.cpp
@@ -0,0 +1,82 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<set>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+#define B_ct(n) __builtin_popcountll(n)
+const ll  mod = 998244353;
+const ll  mod2 = 2e9+7;
+
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 51;
+char a[N][N];
+
+int n, m;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)cin >> a[i][j];
+		}
+		for (int i=n-1;i>=0;i--)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (a[i][j] == '*')
+				{
+					int x = i+1;
+					while (x < n && a[x][j] == '.')x++;
+					a[i][j] = '.';
+					a[x-1][j] = '*';
+				//	cout <<i<<' '<< x; el
+				}
+			}
+		}
+		
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				cout << a[i][j] ;
+			}
+			el
+		}
+	}
+	return 0;
+}

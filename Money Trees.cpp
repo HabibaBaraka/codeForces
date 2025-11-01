9856a551652ed7c7989af5152187e10711d5b625
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-11-01 23:26:35 +0200

    Add Money Trees.cpp with main logic implementation

---
+++ Money Trees.cpp
@@ -0,0 +1,80 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<string>
+#include<algorithm>
+#include<set>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "YES\n"
+#define O "NO\n"
+#define el cout<<'\n';
+#define B_ct(n) __builtin_popcountll(n)
+const ll  mod = 998244353;
+const ll  mod2 = 1e9 + 7;
+#define gr vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 25+9;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		ll k;
+		cin >> k;
+		vector<ll>a(n), h(n);
+		for (auto& it : a)cin >> it;
+		for (auto& it : h)cin >> it;
+		int l = 0, r = 0,mx=0;
+		while (l < n)
+		{
+			ll ans = 0;
+			bool f = 0;
+			r = l;
+			while (r < n)
+			{
+				if (ans > 0)if (h[r - 1] % h[r] != 0)break;
+				
+					mx = max(mx, r - l);
+					while (l < r&&ans+a[r]>k)
+					{
+						ans -= a[l];
+						l++;
+					}
+					if (ans + a[r] > k)break;
+					else ans += a[r];
+				r++;
+			}
+			mx = max(mx, r - l);
+			if (l == r)r++;
+			l = r;
+		}cout << mx;el
+	}
+	return 0;
+}

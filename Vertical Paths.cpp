08dd258219658fc01fcf1c6599bce19c087f0bb2
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2025-11-05 18:02:11 +0200

    Create Vertical Paths.cpp

---
+++ Vertical Paths.cpp
@@ -0,0 +1,84 @@
+/*
+	The Cow (2:28)
+
+كَيْفَ تَكْفُرُونَ بِٱللَّهِ وَكُنتُمْ أَمْوَٰتًۭا فَأَحْيَـٰكُمْ ۖ ثُمَّ يُمِيتُكُمْ ثُمَّ يُحْيِيكُمْ ثُمَّ إِلَيْهِ تُرْجَعُونَ ٢٨
+
+How can you deny Allah? You were lifeless and He gave you life,
+then He will cause you to die and again bring you to life,
+and then to Him you will ˹all˺ be returned.
+*/
+#include<vector>
+#include <iostream>
+#include<climits>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<cmath>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<bitset>
+#include<numeric>
+using namespace std;
+#define ll long long
+#define ld long double
+#define Y "Y\n"
+#define O "N\n"
+#define el cout<<'\n';
+#define B_ct(n) __builtin_popcountll(n)
+const ll  mod = 998244353;
+const ll  mod2 = 1e9 + 7;
+#define Graph vector<vector<int>>
+const int dx[] = { 0,0,-1,1,-1,1,1,-1 };
+const int dy[] = { 1,-1,0,0 ,-1,1,-1,1 };
+const char dir[] = { 'R','L','F','D' };
+const int N = 200000+9;
+bool vis[N];
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		vector<int>p(n+1);
+		set<int>s;
+		for (int i = 1; i <= n; i++)s.insert(i);
+
+		for (int i = 1; i <= n; i++)
+		{
+		  vis[i]=0;
+			cin >> p[i];
+			s.erase(p[i]);
+		}
+		if(n==1){cout<<1<<'\n'<<1<<'\n'<<1<<'\n';continue;}
+		
+		cout << s.size()<<'\n';
+		for (auto it : s)
+		{
+			vector<int>path;
+			path.push_back(it);
+			int x = it;
+			while (!vis[p[x]] && p[x] != x)
+			{
+				path.push_back(p[x]);
+			//	cout << p[x];el
+				vis[p[x]] = 1;
+				x = p[x];
+			}
+			
+			reverse(path.begin(), path.end());
+			cout << path.size();
+			el;
+			for (auto itt : path)cout << itt << " ";
+			el
+		}
+		
+	}
+	return 0;
+}

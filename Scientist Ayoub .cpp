2c6b01590d3d36eecca6e6f27ea1724f631a8a83
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2026-01-23 21:07:42 +0200

    Create Scientist Ayoub .cpp

---
+++ Scientist Ayoub .cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define ll long long 
+const int N = 1e4;
+#include<set>
+int a[101],n;
+
+int main()
+{
+	int tc = 1;
+	//cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		ll x = 1e9;
+		while (n--)cout << x-- << ' ';
+	}
+	return 0;
+}

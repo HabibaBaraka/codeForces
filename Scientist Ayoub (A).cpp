e6d8d3caaaeac1fb6ac10776bdbee957a662ee8c
HabibaBaraka <bdalmnmbrkh274@gmail.com>
2026-01-23 21:04:49 +0200

    Add initial implementation of Scientist Ayoub program

---
+++ Scientist Ayoub (A).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define ll long long 
+const int N = 1e5;
+#include<algorithm>
+int a[101],n;
+
+int main()
+{
+	int tc = 1;
+	//cin >> tc;
+	while (tc--)
+	{
+		int n;cin >> n;
+		for (int i = 0;i < n;i++)cout << (1 << i)<<' ';
+
+	}
+	return 0;
+}
